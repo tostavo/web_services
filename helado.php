e40ca2a1e2d6b67ba0407741fb95a95c39bd2f6e
Gustavo <157194078+tostavo@users.noreply.github.com>
2025-04-11 09:01:58 -0500

    Add files via upload

--- helado.php
+++ helado.php
@@ -4,19 +4,43 @@ class Helado {
     var $altura;
     var $sabor;
     var $precio;
+    var $tipo;
     
-    function Helado() {
-        $this->altura = 15; // en cm
-        $this->sabor = "vainilla";
-        $this->precio = 2.50;
-        echo 'Altura: ' . $this->altura . ' cm<br>';
-        echo 'Sabor: ' . $this->sabor . '<br>';
-        echo 'Precio: $' . $this->precio . '<br>';
+    function Helado($altura = 15, $sabor = "vainilla", $precio = 2.50, $tipo = "cono") {
+        $this->altura = $altura;
+        $this->sabor = $sabor;
+        $this->precio = $precio;
+        $this->tipo = $tipo;
+    }
+    
+    function mostrarInfo() {
+        echo "Helado de {$this->sabor}, tipo {$this->tipo}, altura {$this->altura} cm, precio \${$this->precio}<br>";
+    }
+    
+    function derretir() {
+        $this->altura *= 0.7;
+        echo "El helado se ha derretido. Nueva altura: {$this->altura} cm<br>";
+    }
+    
+    function cambiarSabor($nuevoSabor) {
+        $this->sabor = $nuevoSabor;
+    }
+    
+    function calcularPrecioConIva() {
+        return $this->precio * 1.16;
     }
 }
 
-$miHelado = new Helado();
+// Método main() que crea objetos de ambas clases
+function main() {
+    
+    // Crear objeto Helado
+    $helado1 = new Helado(20, "chocolate", 3.75, "copa");
+    
+    $helado1->mostrarInfo(); //
+}
 
-echo = Helado;
+// Ejecutar el método main
+main();
 
 ?>
